d940670eeb171963c1364534431162ad1b91db9b
Thiago Mota <th.araujomota@gmail.com>
2022-09-09 17:19:17 -0300

    Cria migrations do PostCategories

---
+++ src/database/migrations/20220909193246-PostCategories.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
