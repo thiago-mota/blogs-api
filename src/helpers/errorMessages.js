9e75a895c22890bbfdc20c79af75c58b79595c5a
Thiago Mota <th.araujomota@gmail.com>
2022-09-10 01:37:16 -0300

    Adiciona novas mensagens e codigos de status e erros

--- src/helpers/errorMessages.js
+++ src/helpers/errorMessages.js
@@ -5,6 +5,8 @@ const errors = {
   INVALID_EMAIL: '"email" must be a valid email',
   INVALID_PASSWORD_LENGTH: '"password" length must be at least 6 characters long',
   USER_ALREADY_REGISTERED: 'User already registered',
+  TOKEN_NOT_FOUND: 'Token not found',
+  INVALID_OR_EXPIRED_TOKEN: 'Expired or invalid token',
 };
 
 module.exports = { errors };
--- src/helpers/statusMessages.js
+++ src/helpers/statusMessages.js
@@ -2,6 +2,7 @@ const status = {
   OK_200: 200,
   CREATED_201: 201,
   BAD_REQUEST_400: 400,
+  UNAUTHORIZED_401: 401,
   CONFLICT_409: 409,
 };
 
