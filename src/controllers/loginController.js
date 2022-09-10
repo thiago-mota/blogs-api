9dc5b1bafeb7e7d85b24d2708665c7bca8c0338e
Thiago Mota <th.araujomota@gmail.com>
2022-09-09 23:25:17 -0300

    Adiciona loginController

---
+++ src/controllers/loginController.js
@@ -0,0 +1,20 @@
+const { createUser } = require('../services/loginService');
+const { status } = require('../helpers/statusMessages');
+const createToken = require('../helpers/createToken');
+
+const login = async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    const { id } = await createUser(email, password);
+
+    return response
+    .status(status.OK_200)
+    .json({ token: createToken(id) });
+  } catch (error) {
+    return response
+    .status(status.BAD_REQUEST_400)
+    .json({ message: error.message });
+  }
+};
+
+module.exports = login;
