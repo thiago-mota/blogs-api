3b99d8ca606acc7131f90f95d3d837039575ec47
Thiago Mota <th.araujomota@gmail.com>
2022-09-10 01:23:15 -0300

    Adiciona novos status e error messages

--- src/helpers/errorMessages.js
+++ src/helpers/errorMessages.js
@@ -1,6 +1,10 @@
 const errors = {
   MISSING_FIELDS: 'Some required fields are missing',
   INVALID_FIELDS: 'Invalid fields',
+  INVALID_DISPLAYNAME_LENGTH: '"displayName" length must be at least 8 characters long',
+  INVALID_EMAIL: '"email" must be a valid email',
+  INVALID_PASSWORD_LENGTH: '"password" length must be at least 6 characters long',
+  USER_ALREADY_REGISTERED: 'User already registered',
 };
 
 module.exports = { errors };
--- src/helpers/statusMessages.js
+++ src/helpers/statusMessages.js
@@ -1,6 +1,8 @@
 const status = {
   OK_200: 200,
+  CREATED_201: 201,
   BAD_REQUEST_400: 400,
+  CONFLICT_409: 409,
 };
 
 module.exports = { status };
