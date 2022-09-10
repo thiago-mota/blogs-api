dc9c63a8d5302e16095f5298f5e37d420d458a13
Thiago Mota <th.araujomota@gmail.com>
2022-09-10 02:25:33 -0300

    Corrige import de validação

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -1,21 +1,24 @@
 const { errors } = require('../helpers/errorMessages');
 const { status } = require('../helpers/statusMessages'); 
+const { decodeToken } = require('../helpers/createToken');
 
 const tokenValidation = (request, response, next) => {
   const { authorization } = request.headers;
 
   try {
-  if (!authorization) {
+    if (!authorization) {
+      return response
+        .status(status.UNAUTHORIZED_401)
+        .json({ message: errors.TOKEN_NOT_FOUND });
+    }
+    request.user = decodeToken(authorization);
+  } catch (error) {
     return response
       .status(status.UNAUTHORIZED_401)
-      .json(errors.TOKEN_NOT_FOUND);
+      .json({ message: errors.INVALID_OR_EXPIRED_TOKEN });
   }
+
   next();
-} catch (error) {
-  return response
-    .status(status.UNAUTHORIZED_401)
-    .json(errors.INVALID_OR_EXPIRED_TOKEN);
-}
 };
 
 module.exports = { tokenValidation };
