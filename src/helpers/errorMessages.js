fc22f3489f875e5b768afeb82fc1d3afd7a89672
Thiago Mota <th.araujomota@gmail.com>
2022-09-10 21:53:48 -0300

    Corrige mensagem name is required

--- src/helpers/errorMessages.js
+++ src/helpers/errorMessages.js
@@ -8,6 +8,7 @@ const errors = {
   USER_DOES_NOT_EXIST: 'User does not exist',
   TOKEN_NOT_FOUND: 'Token not found',
   INVALID_OR_EXPIRED_TOKEN: 'Expired or invalid token',
+  NAME_IS_REQUIRED: '"name" is required',
 };
 
 module.exports = { errors };
