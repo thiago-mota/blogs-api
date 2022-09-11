266aa3e624489fbedf5880b64662ec3b967c8c2e
Thiago Mota <th.araujomota@gmail.com>
2022-09-11 19:31:57 -0300

    Adiciona nova mensagem de erro

--- src/helpers/errorMessages.js
+++ src/helpers/errorMessages.js
@@ -9,6 +9,7 @@ const errors = {
   TOKEN_NOT_FOUND: 'Token not found',
   INVALID_OR_EXPIRED_TOKEN: 'Expired or invalid token',
   NAME_IS_REQUIRED: '"name" is required',
+  POST_DOES_NOT_EXIST: 'Post does not exist',
 };
 
 module.exports = { errors };
