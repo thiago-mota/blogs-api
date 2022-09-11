260026ce23e50f7198a41ebfc8426069dd241fbd
Thiago Mota <th.araujomota@gmail.com>
2022-09-11 19:33:04 -0300

    Adiciona findByPk service e controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -14,4 +14,19 @@ const getAllPosts = async (request, response) => {
   }
 };
 
-module.exports = { getAllPosts };
+const getPost = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await postService.getPost(id);
+
+    return response
+    .status(status.OK_200)
+    .json(result);
+  } catch (error) {
+    return response
+    .status(404)
+    .json({ message: error.message });
+  }
+};
+
+module.exports = { getAllPosts, getPost };
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,5 @@
 const { BlogPost, User, Category } = require('../database/models');
+const { errors } = require('../helpers/errorMessages');
 
 const getAllPosts = async () => {
   const allPosts = await BlogPost.findAll({
@@ -11,4 +12,18 @@ const getAllPosts = async () => {
   return allPosts;
 };
 
-module.exports = { getAllPosts };
+const getPost = async (id) => {
+  const post = await BlogPost.findByPk(id, {
+    include: [{ model: User,
+      as: 'user',
+      attributes: { exclude: ['password'] },
+    },
+    { model: Category, as: 'categories' }],
+    });
+
+    if (!post) throw Error(errors.POST_DOES_NOT_EXIST);
+
+    return post;
+};
+
+module.exports = { getAllPosts, getPost };
