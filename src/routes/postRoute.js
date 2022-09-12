9dfd62f0dff63e7384529994b02eda9e15a3418e
Thiago Mota <th.araujomota@gmail.com>
2022-09-12 14:28:58 -0300

    Adiciona rota delete e search

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllPosts, getPost } = require('../controllers/postController');
+const { getAllPosts, getPost, deletePost } = require('../controllers/postController');
 const { tokenValidation } = require('../middlewares/tokenValidation');
 
 const postRoute = express.Router();
@@ -7,6 +7,7 @@ const postRoute = express.Router();
 postRoute.post('/', tokenValidation);
 postRoute.get('/', tokenValidation, getAllPosts);
 postRoute.get('/:id', tokenValidation, getPost);
-postRoute.delete('/:id', tokenValidation);
+postRoute.delete('/:id', tokenValidation, deletePost);
+postRoute.get('/search', tokenValidation);
 
 module.exports = postRoute;
