5f8e45e33f3e4391c31540dd34d2f08546fdd41b
Thiago Mota <th.araujomota@gmail.com>
2022-09-12 14:27:14 -0300

    Adiciona deletePost e insere as constantes do getPost dentro do try catch

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,8 +2,8 @@ const postService = require('../services/postService');
 const { status } = require('../helpers/statusMessages');
 
 const getAllPosts = async (request, response) => {
-  const result = await postService.getAllPosts();
   try {
+    const result = await postService.getAllPosts();
     return response
       .status(status.OK_200)
       .json(result);
@@ -20,13 +20,29 @@ const getPost = async (request, response) => {
     const result = await postService.getPost(id);
 
     return response
-    .status(status.OK_200)
-    .json(result);
+      .status(status.OK_200)
+      .json(result);
   } catch (error) {
     return response
-    .status(404)
-    .json({ message: error.message });
+      .status(404)
+      .json({ message: error.message });
   }
 };
 
-module.exports = { getAllPosts, getPost };
+const deletePost = async (request, response) => {
+  try {
+    const { id } = request.params;
+    await postService.removePost(id);
+
+    return response
+      .status(204)
+      .json();
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(404)
+      .json({ message: error.message });
+  }
+};
+
+module.exports = { getAllPosts, getPost, deletePost };
