969a0f287279d8100c9ef3644637fb214ac17dab
Thiago Mota <th.araujomota@gmail.com>
2022-09-13 12:18:06 -0300

    Remove rota post do post para verificar andamento do projeto

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { getAllPosts, getPost, deletePost, createPost,
+const { getAllPosts, getPost, deletePost,
 searchPost } = require('../controllers/postController');
 const { tokenValidation } = require('../middlewares/tokenValidation');
-const { validateFields } = require('../middlewares/postValidation');
+// const { validateFields } = require('../middlewares/postValidation');
 
 const postRoute = express.Router();
 
 postRoute.get('/search', tokenValidation, searchPost);
-postRoute.post('/', tokenValidation, validateFields, createPost);
+// postRoute.post('/', tokenValidation, validateFields);
 postRoute.get('/', tokenValidation, getAllPosts);
 postRoute.get('/:id', tokenValidation, getPost);
 postRoute.delete('/:id', tokenValidation, deletePost);
